6f51c1561bcbd1b65e15e707de17139e5d26d059
nather2014 <akhilnather@gmail.com>
2025-11-26 10:03:48 +0530

    auth-service

--- services/auth-service/src/main/java/com/commerce/auth/service/JwtService.java
+++ services/auth-service/src/main/java/com/commerce/auth/service/JwtService.java
@@ -1,6 +1,5 @@
 package com.commerce.auth.service;
 
-import java.security.Key;
 import java.time.Instant;
 import java.util.Date;
 import java.util.Map;
@@ -10,26 +9,36 @@ import org.springframework.stereotype.Service;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
+
+import javax.crypto.SecretKey;
 
 @Service
 public class JwtService {
-  private final Key key = Keys.hmacShaKeyFor(System.getenv().getOrDefault("JWT_SECRET","change-me-very-secret-should-be-32+chars").getBytes());
-  private final long accessTtlSeconds = 60 * 15; // 15 min
+
+  private final SecretKey key = Keys.hmacShaKeyFor(
+      System.getenv().getOrDefault("JWT_SECRET",
+          "change-me-very-secret-should-be-32+chars").getBytes()
+  );
+
+  private final long accessTtlSeconds = 60 * 15;
 
   public String generateAccessToken(Long userId, String email, String rolesCsv) {
     Instant now = Instant.now();
+
     return Jwts.builder()
-      .setSubject(String.valueOf(userId))
-      .setIssuedAt(Date.from(now))
-      .setExpiration(Date.from(now.plusSeconds(accessTtlSeconds)))
-      .addClaims(Map.of("email", email, "roles", rolesCsv))
-      .signWith(key, SignatureAlgorithm.HS256)
-      .compact();
+        .subject(String.valueOf(userId))
+        .issuedAt(Date.from(now))
+        .expiration(Date.from(now.plusSeconds(accessTtlSeconds)))
+        .claims(Map.of("email", email, "roles", rolesCsv))
+        .signWith(key)
+        .compact();
   }
 
   public Jws<Claims> parseToken(String token) {
-    return Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
+    return Jwts.parser()
+        .verifyWith(key)
+        .build()
+        .parseSignedClaims(token);
   }
 }
